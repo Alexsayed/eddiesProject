a7f0ad8285cd14404bff9ad91b3d5570c5a3e02e
Alexsayed <39999280+Alexsayed@users.noreply.github.com>
2025-02-03 09:49:56 -0600

    Add files via upload

--- models/products.ts
+++ models/products.ts
@@ -1,4 +1,7 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
+// import mongoose, { Document } from 'mongoose';
+import mongoose, { Schema, Document, Model } from 'mongoose';
+import Size, { ISizes } from './sizes';
 // export interface Products extends mongoose.Document {
 //   productName: string;
 //   price: number;
@@ -7,9 +10,13 @@ import mongoose from "mongoose";
 //   inStock: boolean;
 //   created: Date;
 // }
-export interface Products {
+
+
+
+export interface Products extends Document {
   _id: string,
-  productName?: string;
+  // quantity: number
+  productName: string;
   price: number;
   productImg: string;
   category: string,
@@ -17,7 +24,8 @@ export interface Products {
   gender: string,
   kids: string,
   color: string,
-  // sizes?: object,
+  // sizes: string[],
+  sizes: ISizes,
   // size: {
   //   menSizes: { pantsOrJeans: boolean, shoe: boolean, tees: boolean, },
   //   womenSizes: { dresses: boolean, pantsOrJeans: boolean, skirts: boolean, shoes: boolean, tops: boolean, }
@@ -25,27 +33,52 @@ export interface Products {
   author: string;
   inStock: boolean;
   created: Date;
-}
 
+}
+// interface Products {
+//   _id: string,
+//   productName: string;
+//   price: number;
+//   productImg: string;
+//   category: string,
+//   brand: string,
+//   gender: string,
+//   kids: string,
+//   color: string,
+//   // sizes: string[],
+//   sizes: ISizes,
+//   // size: {
+//   //   menSizes: { pantsOrJeans: boolean, shoe: boolean, tees: boolean, },
+//   //   womenSizes: { dresses: boolean, pantsOrJeans: boolean, skirts: boolean, shoes: boolean, tops: boolean, }
+//   // },
+//   author: string;
+//   inStock: boolean;
+//   created: Date;
+// }
+// interface ProductsDocument extends mongoose.Document, Products {}
 
 
 const productSchema = new mongoose.Schema<Products>({
+  // const productSchema = new Schema<Products>({
+  // quantity: Number,
+  // _id: String,
   productName: String,
   price: Number,
   productImg: String,
   category: String,
   brand: String,
   gender: String,
-  kids: String,
+  // kids: String,
   color: String,
   // size: String,
-  // sizesHOLD: [
+  // sizes: [
   //   {
   //     type: mongoose.Schema.Types.ObjectId,
   //     ref: "Size"
   //   },
   // ],
-  // sizesHOLD: {
+  sizes: { type: mongoose.Schema.Types.ObjectId, ref: "Size" },
+  // sizesHOLD: { 
   //   type: mongoose.Schema.Types.ObjectId,
   //   ref: "Size",
   // },
@@ -132,12 +165,17 @@ const productSchema = new mongoose.Schema<Products>({
     default: Date.now
   },
 
+
 });
 // var User = mongoose.model("Users", userSchema);
 // export default Users;
 // export default mongoose.models.User || mongoose.model("User", userSchema); 
 // mongoose.models = {};
-export default mongoose.models.Product || mongoose.model<Products>("Product", productSchema);
+// export default mongoose.models.Product || mongoose.model<Products>("Product", productSchema);
+const Product = mongoose.models.Product || mongoose.model<Products>('Product', productSchema);
+// const Product = mongoose.model<Products>('Product', productSchema);
+export default Product;
+// const ProductModel = mongoose.model<ProductsDocument>('Product', productSchema);
 // export default mongoose.model<Products>("Product", productSchema);
 
 // var Product = mongoose.model('Product', productSchema);
